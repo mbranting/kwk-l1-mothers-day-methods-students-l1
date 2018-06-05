111d3e83c77618b3e77400132b29826edfe84b4b
mbranting <mckenna.branting@gmail.com>
2018-06-05 18:40:02 +0000

    automatically backed up by learn

--- variables.rb
+++ variables.rb
@@ -1,8 +1,14 @@
-puts "What is grade?"
-grade = gets.strip
-if "#{grade}" >= "70"
-  puts "Pass"
-else 
-  puts "Fail"
-end
+#puts "What is grade?"
+#grade = gets.strip
+#if "#{grade}" >= "70"
+  #puts "Pass"
+#else 
+  #puts "Fail"
+#end
 
+a = gets.chomp.to.i
+if a > 0
+  puts "number is valid"
+else
+  puts "ERROR"
+end
